e0d000da0274cb85a1da3e9c5576d001539beb3c
Guilherme Assis <guilherme.assis@compasso.com.br>
2021-08-10 16:37:59 -0300

    Documentando API utilizando o Swagger

--- src/main/java/io/guilhermefasilva/api/cadastro/ApiCadastroClienteApplication.java
+++ src/main/java/io/guilhermefasilva/api/cadastro/ApiCadastroClienteApplication.java
@@ -3,7 +3,10 @@ package io.guilhermefasilva.api.cadastro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class ApiCadastroClienteApplication {
 
 	public static void main(String[] args) {
