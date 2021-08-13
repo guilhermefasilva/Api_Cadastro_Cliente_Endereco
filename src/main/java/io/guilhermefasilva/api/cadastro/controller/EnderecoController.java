33fb9e33fe0d870638488871224a2037e7bdb171
Guilherme Assis <guilherme.assis@compasso.com.br>
2021-08-13 17:51:22 -0300

    Atualização na documentação da feature de endereços

--- src/main/java/io/guilhermefasilva/api/cadastro/controller/EnderecoController.java
+++ src/main/java/io/guilhermefasilva/api/cadastro/controller/EnderecoController.java
@@ -35,11 +35,18 @@ public class EnderecoController {
 	
 	@PostMapping
 	@Transactional
+	@ApiOperation(
+			httpMethod = "POST",
+			value = "Persistir endereço na base de dados",
+			response = EnderecoDtoResponse.class,
+			nickname = "postEndereco",
+			notes = "Varios valores de status podem ser fornacidos com strings separadas por virgulas")
 	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Requisição bem sucedida"),
 			@ApiResponse(code = 201, message = "Recurso Criado, requisição bem sucedida"),
 			@ApiResponse(code = 400, message = "Erro de validação, requisição não atendida")
 			})
-	public ResponseEntity<EnderecoDtoResponse> create(@RequestBody @ApiParam("Endereço para cadastro")  EnderecoDtoRequest enderecoRequest) {
+	public ResponseEntity<EnderecoDtoResponse> create(@ApiParam(value = "Valores para cadastrar endereço", required = true) @RequestBody  EnderecoDtoRequest enderecoRequest) {
 		EnderecoDtoResponse enderecoResponse = enderecoService.insert(enderecoRequest);
 
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}").buildAndExpand(enderecoResponse.getId())
@@ -48,7 +55,14 @@ public class EnderecoController {
 	}
 
 	@GetMapping
-	@ApiOperation("Listar todos endereços")
+	@ApiOperation(
+			httpMethod = "GET",
+			value = "Listar todos endereços",
+			response = EnderecoDtoResponse.class,
+			nickname = "findAll",
+			notes = "Não há parametros a serem repassados, apenas a chamada da URi faá com que todos endereços persistidos"
+					+ "\n no banco de dados, sejam retornados no corpo da requisição "
+			)
 	@ApiResponses(value = { 
 			@ApiResponse(code = 200, message = "Requisição bem sucedida"),
 			@ApiResponse(code = 404, message = "Recurso não encontrado"),
@@ -60,11 +74,19 @@ public class EnderecoController {
 	}
 
 	@GetMapping("/{id}")
+	@ApiOperation(
+			httpMethod = "GET",	
+			value ="Pesquisar endereços por Id",
+			response = EnderecoDtoResponse.class,
+			nickname = "getById",
+			notes = "Um id pode ser passado como parametro na URI para que a busca por um endereço especifico seja efetuada."
+					+ "\n A requisição trará o endereço no corpo da resposta caso este exista no banco de dados ")
+	
 	@ApiResponses(value = { 
 			@ApiResponse(code = 200, message = "Requisição bem sucedida"),
 			@ApiResponse(code = 404, message = "Recurso solicitado não encontrado")
 	})
-	public ResponseEntity<EnderecoDtoResponse> findById(@PathVariable @ApiParam("Id do endereço") Long id) {
+	public ResponseEntity<EnderecoDtoResponse> findById(@ApiParam(value = "Id do endereço", required = true)@PathVariable Long id) {
 		EnderecoDtoResponse enderecoResponse = enderecoService.findById(id);
 		return ResponseEntity.ok().body(enderecoResponse);
 
@@ -74,6 +96,13 @@ public class EnderecoController {
 
 	@PutMapping("/{id}")
 	@Transactional
+	@ApiOperation(
+			httpMethod = "PUT",	
+			value ="Atualizar endereços",
+			response = EnderecoDtoResponse.class,
+			nickname = "updateEndereco",
+			notes = "Um id pode ser passado como parametro na URI referenciando o endereço a ser atualizado."
+					+ "No corpo da requisição deverá ser informado os valores para atualização dos campos desejados. ")
 	@ApiResponses(value = { 
 			@ApiResponse(code = 200, message = "Requisição bem sucedida"),
 			@ApiResponse(code = 404, message = "Recurso solicitado não encontrado")
@@ -85,6 +114,11 @@ public class EnderecoController {
 	}
 
 	@DeleteMapping("/{id}")
+	@ApiOperation(
+			httpMethod = "DELETE",	
+			value ="Deletar endereços",
+			nickname = "deleteEndereco",
+			notes = "Um id pode ser passado como parametro na URI referenciando o endereço a ser deletado.")
 	@ApiResponses(value = {
 			@ApiResponse(code = 204, message = "Não há conteudos a ser exibidos ou enviados, requisição bem sucedida"),
 			@ApiResponse(code = 400, message = "Recurso não pode ser excluido"),
